d9baeee78497d7a8175fbeb824bae988c28dd04f
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-12 16:15:55 -0500

    :green_heart: Fix CI

--- scripts/node.ts
+++ scripts/node.ts
@@ -2,7 +2,8 @@ import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.14/node_suppor
 
 buildPackage({
   repoName: "drive-link",
-  description: "Generate a Google Drive direct download link based on the URL or ID.",
+  description:
+    "Generate a Google Drive direct download link based on the URL or ID.",
   homepage: "https://ulti.js.org/drive-link",
   keywords: ["drive", "google"],
   version: "1.0.0",
