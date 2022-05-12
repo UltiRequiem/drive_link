9dc64ec010a43b71486ffe3586a6d0fdc1d23ba8
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-12 16:14:04 -0500

    :construction_worker: Add CI

---
+++ scripts/node.ts
@@ -0,0 +1,9 @@
+import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.14/node_support.ts";
+
+buildPackage({
+  repoName: "drive-link",
+  description: "Generate a Google Drive direct download link based on the URL or ID.",
+  homepage: "https://ulti.js.org/drive-link",
+  keywords: ["drive", "google"],
+  version: "1.0.0",
+});
