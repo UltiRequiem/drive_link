8ee13ecc770901239604a10597c8bc9842b9e528
GitHub <noreply@github.com>
2023-02-01 00:22:34 +0000

    ⬆️  Update dependencies

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 import {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.170.0/testing/asserts.ts";
+} from "https://deno.land/std@0.175.0/testing/asserts.ts";
 
 import { driveLink } from "./mod.ts";
 
